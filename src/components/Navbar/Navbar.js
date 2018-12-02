6a7e849215c72c3699b06279c633df8f2a3cc535
Phung Pham <phungpham.tech@gmail.com>
2018-12-02 13:14:35 -0500

    updated routes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,18 +3,16 @@ import './navbar.css';
 import NavTabs from './NavTabs';
 
 const Navbar = () => {
-        return (
-            <div>
-                <nav className="navbar py-0 px-0 navbar-light bg-light">
-                    <div className="container" id="header">
-                        <div className="p-3 mb-0 bg-info text-white" id="logo">
-                            <a className="navbar-brand text-white mx-0 pb-0 font-weight-bold" href="/">PHUNG PHAM</a>
-                        </div>
-                        <NavTabs />
-                    </div>
-                </nav>
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container" id="header">
+                <div className="p-3 mb-0 bg-info text-info" id="logo">
+                    <a className="navbar-brand text-info mx-0 pb-0 font-weight-bold" href="/">PHUNG PHAM</a>
+                </div>
+                <NavTabs />
             </div>
-        )
+        </nav>
+    )
 }
 
 export default Navbar;
