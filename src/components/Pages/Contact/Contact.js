8bb228f5fed936f1a529922191fe80640f6c86ba
Phung Pham <phungpham.tech@gmail.com>
2018-12-09 14:11:04 -0500

    updated contact

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -7,25 +7,39 @@ const Contact = () => {
             <div className='row mx-0' id='title-contact'>
                 <h2 className='mx-4 text-info'>Contact</h2>
             </div>
-            <div className="row mt-2">
-                <div className="col-md-12">
-                    <form className="mx-4">
-                        <div className="form-group">
-                            <label for="name">Name</label>
-                            <input type="text" className="form-control" id="name" placeholder="Enter Your Name" name="Name"></input>
-                        </div>
+            <div className="row mt-2 mb-5 ml-5">
+                <div className="col-md-12" id='contact-phung'>
+                    
+                    <div className='contact-info'>
+                        <p>
+                            <span>Email Address: </span>
+                            <span>
+                                <strong>phung.tech@gmail.com</strong>
+                            </span>
+                        </p>
+                        <p className='float-right'>
+                            <a href='mailto:phung.tech'>
+                                <button type="button" class="btn btn-warning">Send Email</button>
+                            </a>
+                        </p>
+                    </div>
+                    <div className='contact-info'>
+                        <p>
+                            <span>Contact Number: </span>
+                            <span><strong>(407) 779.2731</strong></span>
 
-                        <div className="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" className="form-control" id="email" placeholder="Enter Your Email" name="email"></input>
-                        </div>
-
-                        <div className="form-group">
-                            <label for="message">Message</label>
-                            <textarea className="form-control" rows="5" id="message"></textarea>
-                        </div>
-                        <button type="submit" className="btn btn-info float-right">Submit</button>
-                    </form>
+                        </p>
+                        <p className='float-right'>
+                            <a href='tel:+14077792731'>
+                                <button type="button" class="btn btn-warning">Cal Me</button>
+                            </a>
+                        </p>
+                    </div>
+                    <p className='contact-ifno'>
+                        Thank you for visting my website, 
+                        please feel free to contact me at the above contact information.
+                    </p>
+                   
                 </div>
             </div>
         </div>
